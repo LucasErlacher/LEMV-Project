786c9bd9bb1dc981599e9a4070bf860630b0bc98
Lucas Erlacher Rodrigues <lucas.rodrigues@industriall.ai>
2020-08-01 19:47:21 -0300

    implemented domain notification

--- api/src/LEMV/LEMV.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace LEMV.Domain
-{
-    public class Class1
-    {
-    }
-}
---
+++ api/src/LEMV/LEMV.Domain/Services/BaseService.cs
@@ -0,0 +1,43 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using LEMV.Domain.Interfaces;
+using LEMV.Domain.Notifications;
+
+namespace LEMV.Domain.Services
+{
+    public abstract class BaseService
+    {
+        private readonly INotificator _notificator;
+
+        public BaseService(INotificator notificator)
+        {
+            _notificator = notificator;
+        }
+
+        protected void Notify(string message)
+        {
+            _notificator.Handle(new Notification(message));
+        }
+
+        protected void Notify(ValidationResult validationResult)
+        {
+            var errors = validationResult.Errors;
+
+            foreach (var error in errors)
+            {
+                Notify(error.ErrorMessage);
+            }
+        }
+
+        protected bool Validate<TEntity>(AbstractValidator<TEntity> validator, TEntity entity)
+        {
+            var result = validator.Validate(entity);
+
+            if (result.IsValid) return true;
+
+            Notify(result);
+
+            return false;
+        }
+    }
+}
