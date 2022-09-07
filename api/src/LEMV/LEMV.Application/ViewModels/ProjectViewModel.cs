1e61790b043bb5f8975d4372d81dd3bbee1c8855
Lucas Erlacher Rodrigues <lucaserlacher@hotmail.com>
2022-09-07 19:51:53 -0300

    implementa imagem para projeto

--- api/src/LEMV/LEMV.Application/ViewModels/ProjectViewModel.cs
+++ api/src/LEMV/LEMV.Application/ViewModels/ProjectViewModel.cs
@@ -8,8 +8,8 @@ namespace LEMV.Application.ViewModels
         public string Titulo { get; set; }
         public string Descricao { get; set; }
         public string NomeAutor { get; set; }
-        public string UrlImagem { get; set; }
         public string Texto { get; set; }
+        public MediaInfoViewModel Media { get; set; }
         public ICollection<ProjectStepViewModel> Manual { get; set; }
         public SkillViewModel Skill { get; set; }
     }
