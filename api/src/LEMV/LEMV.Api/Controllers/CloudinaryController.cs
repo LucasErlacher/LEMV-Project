d3e57e43aafbc732c09a6f4f8a0fe79f5163ab1b
ViniciusFR <viniciusrocha28@gmail.com>
2023-03-06 20:00:44 -0300

    feat(cloudinary): add feature cloudinary to upload image

---
+++ api/src/LEMV/LEMV.Api/Controllers/CloudinaryController.cs
@@ -0,0 +1,26 @@
+﻿using LEMV.Application.Services.Cloudinary;
+using Microsoft.AspNetCore.Mvc;
+using LEMV.Application.Services.Interfaces;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace LEMV.Api.Controllers
+{
+    public class CloudinaryController : ControllerBase
+{
+        private readonly ICloudinaryService _cloudinaryService;
+
+        public CloudinaryController(ICloudinaryService cloudinaryService)
+        {
+            _cloudinaryService = cloudinaryService;
+        }
+
+        [HttpPost("upload")]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            var result = await _cloudinaryService.UploadAsync(file);
+
+            return Ok(result);
+        }
+    }
+}
--- api/src/LEMV/LEMV.Domain/Entities/Project.cs
+++ api/src/LEMV/LEMV.Domain/Entities/Project.cs
@@ -17,7 +17,6 @@ namespace LEMV.Domain.Entities
 
         public List<string> Tags { get; set; }
 
-
         public Project() { }
 
         public Project(
